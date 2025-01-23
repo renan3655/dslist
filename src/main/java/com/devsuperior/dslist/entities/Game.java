2ed08da0cfba607c0d98bef3012f0686814805da
Renan Quintanilha <renan_m.ice@hotmail.com>
2025-01-23 20:33:35 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -12,32 +12,32 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	
+
 	@Column(name = "game_year")
 	private Integer year;
-	
+
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		
+
 		this.id = id;
 		this.title = title;
 		this.year = year;
@@ -88,7 +88,7 @@ public class Game {
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
 	}
-	
+
 	public Double getScore() {
 		return score;
 	}
@@ -137,6 +137,5 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
