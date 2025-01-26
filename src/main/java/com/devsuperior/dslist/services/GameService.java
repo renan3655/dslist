2fa271a6209d0be277b670ef73612e26994ae503
Renan Quintanilha <renan_m.ice@hotmail.com>
2025-01-26 09:53:18 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 import jakarta.transaction.Transactional;
@@ -17,19 +18,26 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public GameDTO findById(long id) {
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
 	}
-	
+
 	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
-		
+
+	}
+
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+
 	}
 }
